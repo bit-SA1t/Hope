cc75adbfc6f3d19ffad9c0332b1576e1d25796d8
bit-SA1t <56808222+bit-SA1t@users.noreply.github.com>
2019-10-21 09:32:11 +0800

    Hello java

---
+++ helloJava.java
@@ -0,0 +1,16 @@
+/**
+  这是我的第一java程序，hellojava案例。
+ */
+
+
+public class helloJava {//这是定义一个hellojava类，Java中最小的单位就是类
+    /**
+     main方法是程序的主入口，写法固定
+     */
+    public static void main(String[]args){
+        /*
+        这是main方法的主体，我们要实现的功能都在这里面
+         */
+        System.out.println("helloJava");//这是一个输出语句，可以将输出语句打印到屏幕
+    }
+}
